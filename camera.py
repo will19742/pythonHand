2d07daef57000204db31e8b8b61824067f48fa5a
will19742 <73005677+will19742@users.noreply.github.com>
2024-08-09 16:07:47 -0600

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,36 +2,29 @@ import cv2
 import controller as cnt
 from cvzone.HandTrackingModule import HandDetector
 
-detector=HandDetector(detectionCon=0.8,maxHands=2)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-video=cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
 
 while True:
-    ret,frame=video.read()
-    frame=cv2.flip(frame,1)
-    hands,img=detector.findHands(frame)
+    ret, frame = video.read()
+    frame = cv2.flip(frame, 1)
+    hands, img = detector.findHands(frame)
+
     if hands:
-        lmList=hands[0]
-        fingerUp=detector.fingersUp(lmList)
+        lmList = hands[0]
+        fingerUp = detector.fingersUp(lmList)
 
         print(fingerUp)
         cnt.led(fingerUp)
-        if fingerUp==[0,0,0,0,0]:
-            cv2.putText(frame,'No Dedos:0',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,0,0,0]:
-            cv2.putText(frame,'No Dedos:1',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)    
-        elif fingerUp==[0,1,1,0,0]:
-            cv2.putText(frame,'No Dedos:2',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,1,1,0]:
-            cv2.putText(frame,'No Dedos:3',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[0,1,1,1,1]:
-            cv2.putText(frame,'No Dedos:4',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        elif fingerUp==[1,1,1,1,1]:
-            cv2.putText(frame,'No Dedos:5',(20,460),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1,cv2.LINE_AA) 
 
-    cv2.imshow("frame",frame)
-    k=cv2.waitKey(1)
-    if k==ord("k"):
+        # Mostrar el número de dedos levantados
+        numFingers = fingerUp.count(1)
+        cv2.putText(frame, f'Dedos Levantados: {numFingers}', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord("k"):
         break
 
 video.release()
